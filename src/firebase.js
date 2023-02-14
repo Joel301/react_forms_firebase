0d6490ae97ee7509b5015408677219df57d5ce17
Joel301 <joel3.1416@gmail.com>
2023-02-14 12:49:24 -0600

    enviroment debug

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { addDoc, getFirestore } from "firebase/firestore"
+import firebaseConfigObject from "./firebase.config.json"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-if (REACT_APP_FIREBASECONFIG) {
-   firebaseConfig = JSON.parse(REACT_APP_FIREBASECONFIG)
-} else {
-   import firebaseConfig from "./firebase.config.json"
-}
-
 // Initialize Firebase
-initializeApp(firebaseConfig);
+initializeApp(
+   REACT_APP_FIREBASECONFIG                     //firebase config from enviroment
+      ? JSON.parse(REACT_APP_FIREBASECONFIG)
+      : firebaseConfigObject);                  //firebase config from local json
 export const db = getFirestore()
